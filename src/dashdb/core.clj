c4a78d544ef3f901d8be0ee80953ad19f7c488ea
Dan Rauch <drauch@hawk.iit.edu>
2018-12-12 17:11:41 -0600

    CREATE query parsing

--- src/dashdb/core.clj
+++ src/dashdb/core.clj
@@ -1,17 +1,15 @@
 (ns dashdb.core
-  (:require [dashdb.file :as f])
+  (:require [dashdb.fs :as fs]
+            [dashdb.query :as q]
+            [clojure.string :as str])
   (:gen-class))
 
 (defn -main
   "Control HQ"
   [& args]
-  (def file {:name "resources/t1" :type "inode"})
-  (loop [x 100]
-    (when (> x 1)
-      (f/write-file (merge file {:content (rand-int 100)}))
-      (f/write-file (merge file {:content "\n"}))
-      (recur (- x 1))))
-  (f/read-nth-block (merge file {:number 7}))
-  )
-
-(-main)
+  (loop []
+    (do (print "dash=> ") (flush)
+      (let [input (read-line)]
+        (if (not (= input "exit"))
+          (do (q/parse-input (str/trim input)) (recur))
+          )))))
--- src/dashdb/fs.clj
+++ src/dashdb/fs.clj
@@ -1,29 +1,31 @@
-;;;; File system implementing transations as inodes, or TAIN
+;;;; File read/write system for graph directory files.
+;;;; Using a strategy called TAIN, or transactions as inodes.
 
-(ns dashdb.file
+(ns dashdb.fs
   (:require [clojure.java [io :as io]]))
 
-(defn write-file
-  "Append content to a file."
-  [{:keys [name content] :as file}]
-  (locking file (spit name content :append true)))
-
-(defn get-file
-  "Gets the contents of a file. Will eventually be abandoned."
-  [{:keys [name] :as file}]
-  (locking file (slurp name)))
-
-(defn file-bytes
-  "Copies contents of a file to xout java.io.ByteArrayOutputStream and then outputs a java ByteArray. Too slow because of io/copy obviously."
+(defn read-from-file
+  "Read file into byte array and return the byte array."
   [{:keys [name] :as file}]
   (locking file
-    (with-open [xin (io/input-stream name)
-                xout (java.io.ByteArrayOutputStream.)]
-      (io/copy xin xout)
-      (.toByteArray xout))))
+    (let [f (java.io.File. name)
+          ary (byte-array (.length f))
+          is (java.io.FileInputStream. f)]
+      (.read is ary)
+      (.close is)
+      (assoc file :contents (vec ary)))))
 
-(defn read-nth-block
-  "Read line-number from the given text file. The first line has the number 1. Too slow because of lazy seq creating by line-seq"
-  [{:keys [name number] :as file}]
-  (with-open [rdr (clojure.java.io/reader name)]
-    (nth (line-seq rdr) (dec number))))
+(defn append-content
+  "Write content(string) to the file map and return that map."
+  [file contents]
+  (locking file
+    (update-in file [:contents] #(vec (concat % (map byte contents))))))
+
+(defn write-to-file
+  "Write byte array to file."
+  [{:keys [name contents] :as file}]
+  (locking file
+    (let [f (java.io.File. name)
+          is (java.io.FileOutputStream. f)]
+      (.write is (byte-array contents))
+      (.close is))))
