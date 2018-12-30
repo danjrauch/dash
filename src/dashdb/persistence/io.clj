c899c6a8441a3f50e3d8627449b116042a88aa58
Dan Rauch <drauch@hawk.iit.edu>
2018-12-30 01:20:33 -0600

    File Protocol

--- src/dashdb/persistence/io.clj
+++ src/dashdb/persistence/io.clj
@@ -4,37 +4,49 @@
 (ns dashdb.persistence.io
   (:require [clojure.string :as str]))
 
-(defn bytes->num 
+(defprotocol File
+  (get-name [this])
+  (get-contents [this])
+  (read-from-file [this])
+  (write-to-file [this])
+  (append-content [this contents])
+  (concat-append [this args]))
+
+;(assoc file :contents (vec ary))
+
+(defn create-file
+  "Create a new File"
+  [name]
+  (let [file (java.util.HashMap. {:name name :contents (java.util.ArrayList.)})]
+    (reify
+      File
+      (get-name [_] (.get ^java.util.HashMap file :name))
+      (get-contents [_]
+        (locking file
+          (vec (.get ^java.util.HashMap file :contents))))
+      (read-from-file [_]
+        (locking file
+          (let [f (java.io.File. (.get ^java.util.HashMap file :name))
+                ary (byte-array (.length f))
+                is (java.io.FileInputStream. f)]
+            (.read is ary)
+            (.close is)
+            (.put ^java.util.HashMap file :contents (java.util.ArrayList. (vec ary)))
+            )))
+      (append-content [_ contents]
+        (locking file
+         (.addAll (.get ^java.util.HashMap file :contents) (java.util.ArrayList. (vec (map byte contents))))))
+      (concat-append [this args]
+        (locking file
+          (append-content this (str/join args))))
+      (write-to-file [_]
+        (locking file
+          (let [f (java.io.File. name)
+                is (java.io.FileOutputStream. f)]
+            (.write is (byte-array (.get ^java.util.HashMap file :contents)))
+            (.close is))))
+      )))
+
+(defn bytes->num
   [data]
   (reduce bit-or (map-indexed (fn [i x] (bit-shift-left (bit-and x 0x0FF) (* 8 (- (count data) i 1)))) data)))
-
-(defn read-from-file
-  "Read file into byte array and return the byte array."
-  [{:keys [name] :as file}]
-  (locking file
-    (let [f (java.io.File. name)
-          ary (byte-array (.length f))
-          is (java.io.FileInputStream. f)]
-      (.read is ary)
-      (.close is)
-      (assoc file :contents (vec ary)))))
-
-(defn concat-content
-  "Concat the contents of a record into dash format."
-  [& args]
-  (str/join args))
-
-(defn append-content
-  "Write content(string) to the file map and return that map."
-  [file contents]
-  (locking file
-    (update-in file [:contents] #(vec (concat % (map byte contents))))))
-
-(defn write-to-file
-  "Write byte array to file."
-  [{:keys [name contents] :as file}]
-  (locking file
-    (let [f (java.io.File. name)
-          is (java.io.FileOutputStream. f)]
-      (.write is (byte-array contents))
-      (.close is))))
